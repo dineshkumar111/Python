215f1f1b906e92b6723e1b7c6ab45fd61e4008b1
dineshkumar111 <84616658+dineshkumar111@users.noreply.github.com>
2021-05-31 20:59:29 +0530

    Create elif.py

---
+++ elif.py
@@ -0,0 +1,19 @@
+x = 1
+
+if x==1 :
+    print('one')
+
+elif x==2 :
+    print('two')
+
+elif x==3 :
+    print('three')
+
+elif x==4 :
+    print('four')   
+
+    
+    
+    
+#sample output
+one
