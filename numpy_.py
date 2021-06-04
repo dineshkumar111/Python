344f6613b7b2a0cdd03a1d88ef930ea7eee743b0
dineshkumar111 <84616658+dineshkumar111@users.noreply.github.com>
2021-06-04 21:07:07 +0530

    Create numpy_.py

---
+++ numpy_.py
@@ -0,0 +1,14 @@
+from numpy import *
+import numpy
+
+
+
+s= numpy.array([1,2,3,4,5])
+d = numpy.array([6,2,5,4,8])
+
+c = s + 4
+sum = s + d
+sub = s - d
+
+print(sum,sub)
+print(numpy.sin(s),numpy.log(d))
