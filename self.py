6a957df0c469f35e1e3434e898aece575ae945a9
dineshkumar111 <84616658+dineshkumar111@users.noreply.github.com>
2021-06-10 21:33:08 +0530

    Add files via upload

---
+++ self.py
@@ -0,0 +1,18 @@
+
+
+class computer:
+
+    def __init__(self,name,age):
+
+        self.name=name
+        self.age=age
+
+    
+        
+
+c1=computer("dinesh",19)
+c2=computer("manoj",19)
+
+print(c1.name,c1.age)
+
+print(c2.name,c2.age)
