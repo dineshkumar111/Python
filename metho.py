dde5af72262bff3013b35b4898e400353fba720c
dineshkumar111 <84616658+dineshkumar111@users.noreply.github.com>
2021-06-13 21:00:32 +0530

    Add files via upload

---
+++ metho.py
@@ -0,0 +1,21 @@
+
+
+a = 5
+b = 6
+
+print(a+b)
+
+print(int.__add__(a,b))
+print(int.__sub__(a,b))
+print(int.__mul__(a,b))
+print(int.__divmod__(a,b))
+
+
+
+
+#sample output
+#11
+#11
+#-1
+#30
+#(0, 5)
